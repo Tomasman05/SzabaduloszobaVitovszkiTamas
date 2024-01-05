50441211e2cb8926d53af7bd1a40c0ad44c418bc
Vitovszki Tamás <lolozol80xd@gmail.com>
2024-01-05 16:35:42 +0100

    mukodik :)

--- src/app/uj-foglalas/uj-foglalas.component.ts
+++ src/app/uj-foglalas/uj-foglalas.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { FoglalasService } from '../Services/foglalas.service';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-uj-foglalas',
   templateUrl: './uj-foglalas.component.html',
   styleUrls: ['./uj-foglalas.component.css']
 })
-export class UjFoglalasComponent {
-  adat:any = {
+export class UjFoglalasComponent implements OnInit{
+  adat: any = {
     cim: '',
     datum: '',
     fo: '',
@@ -15,17 +18,35 @@ export class UjFoglalasComponent {
     nev: '',
   };
 
-  constructor(private adatService: FoglalasService) {}
+  constructor(private adatService: FoglalasService, private router: Router) { }
 
-  onSubmit(): void {
-    console.log(this.adat)
-    this.adatService.sendAdat(this.adat).subscribe(
-      (response) => {
-        console.log('Az adat átment!', response);
-      },
-      (error) => {
-        console.error('Hiba történt az adatküldés során! ', error);
-      }
-    );
+  onSubmit(form: NgForm): void {
+    if (form.valid) {
+      console.log(this.adat);
+      this.adatService.sendAdat(this.adat).subscribe(
+        (response) => {
+          console.log('Az adat átment!', response);
+          form.resetForm();
+          this.router.navigate(['/']);
+        },
+        (error) => {
+          console.error('Hiba történt az adatküldés során! ', error);
+        }
+      );
+    } else {
+      alert('Az űrlap hibás. Kérlek ellenőrizd az adatokat.');
+    }
+  }
+
+  private formatDate(date: Date): string {
+    const day = date.getDate();
+    const month = date.getMonth() + 1; // Hónapok 0-tól indexelődnek, így +1
+    const year = date.getFullYear();
+
+    return `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
+  }
+
+  ngOnInit(): void {
+    this.adat.datum = this.formatDate(new Date());
   }
 }
